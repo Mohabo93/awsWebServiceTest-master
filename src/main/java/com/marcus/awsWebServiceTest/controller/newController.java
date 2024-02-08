2eed2ba335f968828d7c309178c114a723145ede
Mohabo93 <93mohabo@gafe.molndal.se>
2024-02-08 10:41:58 +0100

    add controller

---
+++ src/main/java/com/marcus/awsWebServiceTest/controller/newController.java
@@ -0,0 +1,16 @@
+package com.marcus.awsWebServiceTest.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class newController {
+
+    @GetMapping("")
+    public String welcome() {
+        return "Startsida";
+    }
+}
